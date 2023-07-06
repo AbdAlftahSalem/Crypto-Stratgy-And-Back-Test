97b69878d33b5125a1fca76fd53cbd5e7c17330f
AbdAlftahSalem <abd321alftah@gmail.com>
2023-07-06 15:24:54 +0300

    Update back test code and add comments

--- back_tests_next_Indicator.py
+++ back_tests_next_Indicator.py
@@ -2,21 +2,35 @@ from util import getChange, getNumByChange
 
 
 def longBackTest(combined_df, ticker: str, frame: str, ema: str):
+    """
+    Perform a long backtest on the combined DataFrame.
+
+    Args:
+        combined_df: The combined DataFrame containing the data.
+        ticker: The ticker symbol.
+        frame: The time frame.
+        ema: The Exponential Moving Average (EMA) to consider.
+
+    Returns:
+        A dictionary containing the backtest results.
+
+    """
     searchProfit = False
     profit = 0
     stop = 0
-
     enterCandle = combined_df.iloc[0]
-    outPut = {"ticker": ticker, "frame": frame, "profitNum": 0, "loseNum": 0, "data": []}
+
+    output = {"ticker": ticker, "frame": frame, "profitNum": 0, "loseNum": 0, "data": []}
 
     for i in range(len(combined_df)):
         enterCandleSearch = combined_df.iloc[i]
 
-        if enterCandleSearch["signal"] == "buy" and not searchProfit and getChange(enterCandleSearch["high"],
-                                                                                   enterCandleSearch[
-                                                                                       "low"]) < 2 and getEMA(ema,
-                                                                                                              enterCandleSearch,
-                                                                                                              "long"):
+        if (
+                enterCandleSearch["signal"] == "buy"
+                and not searchProfit
+                and getChange(enterCandleSearch["high"], enterCandleSearch["low"]) < 2
+                and getEMA(ema, enterCandleSearch, "long")
+        ):
             enterCandle = combined_df.iloc[i]
             profitPCT = getProfit(frame)
             profit = getNumByChange(enterCandle["close"], profitPCT)
@@ -25,35 +39,54 @@ def longBackTest(combined_df, ticker: str, frame: str, ema: str):
 
         if searchProfit:
             if combined_df.iloc[i]["high"] >= profit:
-                outPut["data"].append(getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "tp", frame))
-                outPut["profitNum"] += 1
+                output["data"].append(
+                    getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "tp", frame)
+                )
+                output["profitNum"] += 1
                 searchProfit = False
                 continue
 
             elif combined_df.iloc[i]["low"] <= stop:
-                outPut["data"].append(getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "sl", frame))
-                outPut["loseNum"] += 1
+                output["data"].append(
+                    getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "sl", frame)
+                )
+                output["loseNum"] += 1
                 searchProfit = False
                 continue
 
-    return outPut
+    return output
 
 
 def sellBackTest(combined_df, ticker: str, frame: str, ema: str):
+    """
+    Perform a sell backtest on the combined DataFrame.
+
+    Args:
+        combined_df: The combined DataFrame containing the data.
+        ticker: The ticker symbol.
+        frame: The time frame.
+        ema: The Exponential Moving Average (EMA) to consider.
+
+    Returns:
+        A dictionary containing the backtest results.
+
+    """
     searchProfit = False
     profit = 0
     stop = 0
     enterCandle = combined_df.iloc[0]
 
-    outPut = {"ticker": ticker, "frame": frame, "profitNum": 0, "loseNum": 0, "data": []}
+    output = {"ticker": ticker, "frame": frame, "profitNum": 0, "loseNum": 0, "data": []}
 
     for i in range(len(combined_df)):
         enterCandleSearch = combined_df.iloc[i]
-        if enterCandleSearch["signal"] == "sell" and not searchProfit and getChange(enterCandleSearch["high"],
-                                                                                    enterCandleSearch[
-                                                                                        "low"]) < 2 and getEMA(ema,
-                                                                                                               enterCandleSearch,
-                                                                                                               "short"):
+
+        if (
+                enterCandleSearch["signal"] == "sell"
+                and not searchProfit
+                and getChange(enterCandleSearch["high"], enterCandleSearch["low"]) < 2
+                and getEMA(ema, enterCandleSearch, "short")
+        ):
             enterCandle = combined_df.iloc[i]
             profitPCT = getProfit(frame)
             profit = getNumByChange(enterCandle["close"], profitPCT * -1)
@@ -62,23 +95,37 @@ def sellBackTest(combined_df, ticker: str, frame: str, ema: str):
 
         if searchProfit:
             if combined_df.iloc[i]["low"] <= profit:
-                outPut["data"].append(getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "tp", frame))
-                outPut["profitNum"] += 1
+                output["data"].append(
+                    getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "tp", frame)
+                )
+                output["profitNum"] += 1
                 searchProfit = False
                 i += 1
                 continue
 
             elif combined_df.iloc[i]["high"] >= stop:
-                outPut["data"].append(getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "sl", frame))
-                outPut["loseNum"] += 1
+                output["data"].append(
+                    getTradeData(enterCandle, combined_df.iloc[i], profit, stop, "sl", frame)
+                )
+                output["loseNum"] += 1
                 searchProfit = False
                 i += 1
                 continue
 
-    return outPut
+    return output
 
 
 def getProfit(frame: str):
+    """
+    Get the profit percentage based on the given time frame.
+
+    Args:
+        frame: The time frame.
+
+    Returns:
+        The profit percentage.
+
+    """
     if frame == "15m":
         return 1.5
 
@@ -90,18 +137,45 @@ def getProfit(frame: str):
 
 
 def getTradeData(enterCandle, exitCandle, profit, stop, status, frame):
+    """
+    Get the trade data for a specific trade.
+
+    Args:
+        enterCandle: The enter candle data.
+        exitCandle: The exit candle data.
+        profit: The profit target.
+        stop: The stop loss target.
+        status: The trade status.
+        frame: The time frame.
+
+    Returns:
+        A dictionary containing the trade data.
+
+    """
     return {
-        "entryDate": enterCandle['date'],
-        "outDate": exitCandle['date'],
-        "enterPrice": enterCandle['close'],
+        "entryDate": enterCandle["date"],
+        "outDate": exitCandle["date"],
+        "enterPrice": enterCandle["close"],
         "tp": profit,
         "sl": stop,
         "status": status,
-        "change": getProfit(frame)
+        "change": getProfit(frame),
     }
 
 
 def getEMA(ema: str, enterCandleSearch, trade_type: str):
+    """
+    Check if the current candle satisfies the EMA condition.
+
+    Args:
+        ema: The EMA to consider.
+        enterCandleSearch: The current candle data.
+        trade_type: The trade type (long or short).
+
+    Returns:
+        True if the condition is satisfied, False otherwise.
+
+    """
     if trade_type == "long":
         return enterCandleSearch[ema] <= enterCandleSearch["close"]
     elif trade_type == "short":

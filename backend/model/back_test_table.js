e3779156d62d42986caf69a7ba8588a9e7d993a6
AbdAlftahSalem <abd321alftah@gmail.com>
2023-07-19 16:19:49 +0300

    Add back test table

---
+++ backend/model/back_test_table.js
@@ -0,0 +1,31 @@
+const {Sequelize, DataTypes} = require('sequelize');
+const sequelize = new Sequelize('mysql::memory:');
+
+module.exports = sequelize.define('back_test', {
+    id: {
+        type: DataTypes.BIGINT.UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    ticker: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    interval: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    exchange: {
+        type: DataTypes.STRING(255),
+        allowNull: false
+    },
+    column_5: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    },
+    signals_back_test_id: {
+        type: DataTypes.BIGINT,
+        allowNull: false
+    }
+});

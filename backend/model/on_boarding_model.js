5b245e97c3e3045b7fb22443c6ee1987f7fd7aad
AbdAlftahSalem <abd321alftah@gmail.com>
2023-07-19 16:20:39 +0300

    Add on boarding table

--- backend/model/on_boarding_model.js
+++ backend/model/on_boarding_model.js
@@ -1,31 +1,25 @@
 const {Sequelize, DataTypes} = require('sequelize');
 const sequelize = new Sequelize('mysql::memory:');
 
-module.exports = sequelize.define('back_test_table', {
+module.exports = sequelize.define('on_boarding_table', {
+
     id: {
         type: DataTypes.BIGINT.UNSIGNED,
         primaryKey: true,
         autoIncrement: true,
         allowNull: false
     },
-    ticker: {
+    title: {
         type: DataTypes.STRING(255),
         allowNull: false
     },
-    interval: {
+    description: {
         type: DataTypes.STRING(255),
         allowNull: false
     },
-    exchange: {
+    image: {
         type: DataTypes.STRING(255),
-        allowNull: false
-    },
-    column_5: {
-        type: DataTypes.BIGINT,
-        allowNull: false
-    },
-    signals_back_test_id: {
-        type: DataTypes.BIGINT,
         allowNull: false
     }
+
 });

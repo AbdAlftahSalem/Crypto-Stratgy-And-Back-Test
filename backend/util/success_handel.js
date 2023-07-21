9cf4745f0eb2f47952203855925977235420cddb
AbdAlftahSalem <abd321alftah@gmail.com>
2023-07-21 20:21:07 +0300

    AddMessageInSuccessRes

--- backend/util/success_handel.js
+++ backend/util/success_handel.js
@@ -1,5 +1,5 @@
 function sendSuccessResponse(res, data, statusCode = 200, message = "success") {
-    return res.status(statusCode).json({success: true, data});
+    return res.status(statusCode).json({success: true, message, data});
 }
 
 module.exports = sendSuccessResponse;

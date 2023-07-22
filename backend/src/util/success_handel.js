e94a0216644b74e5cb4b960376efe32a19fb954c
AbdAlftahSalem <abd321alftah@gmail.com>
2023-07-22 20:50:26 +0300

    Remove back end

--- backend/src/util/success_handel.js
+++
@@ -1,5 +0,0 @@
-function sendSuccessResponse(res, data, statusCode = 200, message = "success") {
-    return res.status(statusCode).json({success: true, message, data});
-}
-
-module.exports = sendSuccessResponse;

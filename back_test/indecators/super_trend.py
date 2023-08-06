b8072b7d5164fb1534e748b75cbe4000f52456f7
AbdAlftahSalem <abd321alftah@gmail.com>
2023-08-06 19:44:54 +0300

    Add CCI

--- back_test/indecators/super_trend.py
+++ back_test/indecators/super_trend.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 
 
 def calculateSuperTrend(df: DataFrame, period) -> DataFrame:
-    superTrendValues = ta.supertrend(df['high'], df['low'], df['close'], period=period, multiplier=3)
+    superTrendValues = ta.supertrend(df['high'], df['low'], df['close'], period=period)
     df['superTrend'] = superTrendValues['SUPERT_3_7.0']
     df['superTrendDirection'] = superTrendValues['SUPERTd_3_7.0']
     return df

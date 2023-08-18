f37bd0af64e8abac5c9519f3eff72229a362c5b1
AbdAlftahSalem <abd321alftah@gmail.com>
2023-08-18 17:40:09 +0300

    Add Short method

--- strategies/hit_and_run/back_test_hit_and_run.py
+++ strategies/hit_and_run/back_test_hit_and_run.py
@@ -1,5 +1,5 @@
 from utils.util import getNumByChange
-from utils.util_back_test import getProfit, getTradeData, getStopLose
+from utils.util_back_test import getProfit, getStopLose
 
 
 def longBackTest(df, ticker: str, frame: str):
@@ -76,51 +76,67 @@ def sellBackTest(combined_df, ticker: str, frame: str):
         ticker: The ticker symbol.
         frame: The time frame.
 
-
     Returns:
-        A dictionary containing the backtest results.
-
+        A dictionary containing the backtest data.
     """
-    searchProfit = False
-    profit = 0
-    stop = 0
-    enterCandle = combined_df.iloc[0]
-    combined_df["sponge_bob_short"] = combined_df["sponge_bob_long"].fillna(0)
+    combined_df["sponge_bob_short"] = combined_df["sponge_bob_short"].fillna(0)
 
     output = {"ticker": ticker, "frame": frame, "profitNum": 0, "loseNum": 0, "data": []}
 
+    # 1- Add sponge_bob_short condition [ sponge_bob_short != 0 and  sponge_bob_short >= 65 ] & candle is green
+    condition = (combined_df['sponge_bob_short'] != 0) & (combined_df['sponge_bob_short'] >= 65) & (
+            combined_df['close'] > combined_df['open'])
+
+    # 2- Create a new column 'enterPrice' when the condition is true
+    combined_df.loc[condition, 'enterPrice'] = (getNumByChange(combined_df.loc[condition, 'high'], 3)).fillna(0)
+
+    # 3- Calculate target and stop prices based on the entered price
+    combined_df.loc[condition, 'targetPrice'] = (
+        getNumByChange(combined_df.loc[condition, 'enterPrice'], -getProfit("hit_and_run", frame, False))).fillna(0)
+    combined_df.loc[condition, 'stopPrice'] = (
+        getNumByChange(combined_df.loc[condition, 'enterPrice'], getStopLose("hit_and_run", False))).fillna(0)
+    # add column strategy to df with true or false values
+    combined_df.loc[condition, 'strategy'] = True
+
     for i in range(len(combined_df)):
-        enterCandleSearch = combined_df.iloc[i]
-        condition = (enterCandleSearch["sponge_bob_short"] != 0 and
-                     enterCandleSearch["mfi"] >= 65 and
-                     enterCandleSearch["rsi"] >= 65
-                     )
-
-        if (
-                condition
-                and not searchProfit
-        ):
+        if combined_df.iloc[i]["strategy"] == True:
             enterCandle = combined_df.iloc[i]
-            profitPCT = getProfit("hit_and_run", frame, False)
-            profit = getNumByChange(enterCandle["close"], profitPCT * -1)
-            stop = getNumByChange(enterCandle["close"], ((profitPCT / getStopLose("hit_and_run", True)) * 1))
-            searchProfit = True
+            targetPrice = combined_df.iloc[i]["targetPrice"]
+            stopPrice = combined_df.iloc[i]["stopPrice"]
 
-        if searchProfit:
-            if combined_df.iloc[i]["low"] <= profit:
-                output["data"].append(
-                    getTradeData("hit_and_run", False, enterCandle, combined_df.iloc[i], profit, stop, "tp", frame))
-                output["profitNum"] += 1
-                searchProfit = False
-                i += 1
-                continue
+            for j in range(i + 1, len(combined_df)):
+                # check if current candle high is greater than or equal enterPrice and candle is green
+                if combined_df.iloc[j]["high"] >= enterCandle["enterPrice"]:
+                    # loop through candles until targetPrice or stopPrice is reached
+                    for k in range(j, len(combined_df)):
+                        # check if current candle low is less than or equal targetPrice
+                        if combined_df.iloc[k]["low"] <= targetPrice:
+                            data = {
+                                "entryDate": enterCandle["date"],
+                                "outDate": combined_df.iloc[k]["date"],
+                                "enterPrice": enterCandle["enterPrice"],
+                                "tp": targetPrice,
+                                "sl": stopPrice,
+                                "status": "tp",
+                                "change": -getProfit("hit_and_run", frame, False),
+                            }
+                            output["data"].append(data)
+                            output["profitNum"] += 1
+                            break
 
-            elif combined_df.iloc[i]["high"] >= stop:
-                output["data"].append(
-                    getTradeData("hit_and_run", False, enterCandle, combined_df.iloc[i], profit, stop, "sl", frame))
-                output["loseNum"] += 1
-                searchProfit = False
-                i += 1
-                continue
+                        # check if current candle high is greater than or equal stopPrice
+                        elif combined_df.iloc[k]["close"] >= stopPrice:
+                            data = {
+                                "entryDate": enterCandle["date"],
+                                "outDate": combined_df.iloc[k]["date"],
+                                "enterPrice": enterCandle["enterPrice"],
+                                "tp": targetPrice,
+                                "sl": stopPrice,
+                                "status": "sl",
+                                "change": getStopLose("hit_and_run", False),
+                            }
+                            output["data"].append(data)
+                            output["loseNum"] += 1
+                            break
 
     return output

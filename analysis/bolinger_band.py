6efe7e667acf982674f22f91c705e3bf81ab134d
AbdAlftahSalem <abd321alftah@gmail.com>
2024-05-27 20:23:59 +0300

    Update BB

--- analysis/bolinger_band.py
+++ analysis/bolinger_band.py
@@ -4,13 +4,13 @@ import pandas_ta as ta
 
 def bollinger_band(df: pd.DataFrame) -> pd.DataFrame:
     bollinger_band_df = ta.bbands(df['close'], length=14)
-    df['BBL'] = bollinger_band_df['BBL_5_2.0']
-    df['BBU'] = bollinger_band_df['BBL_5_2.0']
-    df['BBM'] = bollinger_band_df['BBL_5_2.0']
+    df['BBL'] = bollinger_band_df['BBL_14_2.0']
+    df['BBU'] = bollinger_band_df['BBU_14_2.0']
+    df['BBM'] = bollinger_band_df['BBM_14_2.0']
 
     """
     BBL -> Lower band
-    BBU - > Upper band
+    BBU -> Upper band
     BBM -> Mid band
     """
 

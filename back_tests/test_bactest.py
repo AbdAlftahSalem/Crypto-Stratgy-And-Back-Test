e2994d5fc8a43808ea20ce9136957b51d693d329
AbdAlftahSalem <abd321alftah@gmail.com>
2024-06-26 12:52:08 +0300

    Return VWAP strategy

--- back_tests/test_bactest.py
+++ back_tests/test_bactest.py
@@ -4,9 +4,7 @@ THIS FILE TO TEST AND NEW STRATEGIES OR CHANGE IN ANY PREVIOUS STRATEGY .......,
 
 """
 import pandas
-import pandas_ta
 
-from back_tests.nwe_back_test import nwe_entry
 from back_tests.template_back_test import template_bak_test
 from back_tests.vwap_back_test import vwaps_entry
 from utils.util_back_test import all_statistic
@@ -15,25 +13,27 @@ ticker = 'SOLUSDT'
 interval = '30m'
 strategy = 'vwaps'
 last_ris = None
-support_and_resistant_signals = [0] * 3
 
 df = pandas.read_csv(f'E:\Crypto System\dataIndicator\\{ticker}-{interval}-indicators-new7.csv')
-df['zscore_48'] = pandas_ta.zscore(df['close'], 48)
-df['zscore_200'] = pandas_ta.zscore(df['close'], 200)
-df['zscore_484'] = pandas_ta.zscore(df['close'], 484)
-df['zscore_848'] = pandas_ta.zscore(df['close'], 848)
 
-print("1- FINISH READ DF")
+support_and_resistant = [0] * 3
 
-for x in range(len(df) - 3):
-    current_candle_search = df.iloc[x]
-    prev_candle_search = df.iloc[x - 1]
-    prev_prev_candle_search = df.iloc[x - 2]
-    pre_prev_prev_candle_search = df.iloc[x - 3]
+for candle in range(len(df) - 3):
+    current_candle_search = df.iloc[candle]
+    prev_candle_search = df.iloc[candle - 1]
+    prev_prev_candle_search = df.iloc[candle - 2]
 
-    support_and_resistant_signals.append(vwaps_entry(current_candle_search))
+    if current_candle_search['support_and_resistant'] < 0:
+        last_ris = current_candle_search
+
+    if last_ris is not None:
+        support_and_resistant.append(vwaps_entry(current_candle_search, last_ris))
+    else:
+        support_and_resistant.append(0)
+
+df[f'{strategy}_status_signal'] = support_and_resistant
 
 print(f"2- START BACKTEST FOR {strategy} . {ticker} . {interval}\n\n")
-df[f'{strategy}_status_signal'] = support_and_resistant_signals
+df[f'{strategy}_status_signal'] = support_and_resistant
 data = template_bak_test(df, strategy, ticker, interval)
 all_statistic(data, interval, ticker, strategy)
--- back_tests/vwap_back_test.py
+++ back_tests/vwap_back_test.py
@@ -1,14 +1,9 @@
-def search(zscore48, zscore200, zscore484, zscore848):
-    if zscore200 <= -3 or zscore484 <= -3 or zscore848 <= -3:
-        return True
-    return False
+def vwaps_entry(current_candle, last_ris):
+    long_condition = current_candle['close'] > last_ris['close'] > current_candle['low'] and current_candle[
+        'trend_status'] > 0
 
-
-def vwaps_entry(current_candle):
-    long_condition = search(current_candle['zscore_48'], current_candle['zscore_200'], current_candle['zscore_484'],
-                            current_candle['zscore_848'])
-
-    # long_condition = long_condition and current_candle['vwap50'] > current_candle['vwap100']
+    long_condition = long_condition and current_candle['close'] > current_candle['vwap50'] and current_candle['close'] > \
+                     current_candle['vwap21']
 
     if long_condition:
         return 1
